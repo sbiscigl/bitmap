f26f3a121fe123c415e6b20897ed4e6beff4eb64
Samuel Bisciglia <sbiscigl@Samuels-MacBook-Pro.local>
2017-08-21 19:15:43 -0500

    first commit

---
+++ activitymap.go
@@ -0,0 +1,87 @@
+package activitymap
+
+import "fmt"
+
+/*Tuple tuple type for storing two values*/
+type Tuple struct {
+	a int
+	b int
+}
+
+/*NewTuple constructor for tuple type*/
+func NewTuple(x, y int) Tuple {
+	return Tuple{
+		a: x,
+		b: y,
+	}
+}
+
+/*ActivityMap type, has a single member acitivity which represents a hour of*/
+/*time, each bit indicating if that minute was indeed active                */
+type ActivityMap struct {
+	acitivity uint64
+}
+
+/*New constructor for activity map*/
+func New(mask uint64) *ActivityMap {
+	return &ActivityMap{
+		acitivity: mask,
+	}
+}
+
+/*GetActivty read only accessor for activity map*/
+func (am *ActivityMap) GetActivty() uint64 {
+	return am.acitivity
+}
+
+/*AddMinute adds a entry to the activity map*/
+func (am *ActivityMap) AddMinute(min int) {
+	validateMinute(min)
+	mask := uint64(1) << uint64(min-1)
+	am.acitivity = am.acitivity | mask
+}
+
+/*WasOn returns whether or not a minute is active*/
+func (am *ActivityMap) WasOn(min int) bool {
+	validateMinute(min)
+	mask := uint64(1) << uint64(min-1)
+	if am.acitivity&mask > 0 {
+		return true
+	}
+	return false
+}
+
+/*GetRanges returns all the contiguous ranges of 1's in the mask*/
+func (am *ActivityMap) GetRanges() []Tuple {
+	activeRanges := []Tuple{}
+	prevIndex := 0
+	currentIndex := 0
+	isOn := false
+	for i := 1; i <= 60; i++ {
+		currentIndex = i
+		if am.WasOn(i) {
+			if !isOn {
+				prevIndex = currentIndex
+			}
+			isOn = true
+		} else {
+			if isOn {
+				isOn = false
+				activeRanges = append(activeRanges, NewTuple(prevIndex, currentIndex-1))
+				prevIndex = currentIndex
+			} else {
+				prevIndex = currentIndex
+			}
+		}
+	}
+	if isOn == true {
+		activeRanges = append(activeRanges, NewTuple(prevIndex, currentIndex))
+	}
+	return activeRanges
+}
+
+func validateMinute(min int) {
+	if min > 60 || min <= 0 {
+		panic(fmt.Sprintf("minute out of range, was %v", min))
+	}
+}
---
+++ activitymap_test.go
@@ -0,0 +1,150 @@
+package activitymap
+
+import "testing"
+
+func TestShouldCreateNone(t *testing.T) {
+	if a := New(0); a.GetActivty() != 0 {
+		t.Errorf("should have created none got %v", a.GetActivty())
+	}
+}
+
+func TestShouldSome(t *testing.T) {
+	if a := New(1); a.GetActivty() != 1 {
+		t.Errorf("should have created 1 got %v", a.GetActivty())
+	}
+}
+
+func TestShouldAddMinuteFirstMinute(t *testing.T) {
+	a := New(0)
+	a.AddMinute(1)
+	if a := New(1); a.GetActivty() != 1 {
+		t.Errorf("should have added 1 got %v", a.GetActivty())
+	}
+}
+
+func TestShouldAddMinuteLastMinute(t *testing.T) {
+	a := New(0)
+	a.AddMinute(60)
+	if a.GetActivty() != 576460752303423488 {
+		t.Errorf("should have added 60 minutes (576460752303423488 decimal) got decimal %v", a.GetActivty())
+	}
+}
+
+func TestShouldAddMiddleMinute(t *testing.T) {
+	a := New(0)
+	a.AddMinute(30)
+	if a.GetActivty() != 536870912 {
+		t.Errorf("should have added 30 minutes (536870912 decimal) got decimal %v", a.GetActivty())
+	}
+}
+
+func TestShouldPanicWhenMinuteBad(t *testing.T) {
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("The code did not panic")
+		}
+	}()
+
+	a := New(0)
+	a.AddMinute(61)
+}
+
+func TestShouldVerifyFirstMinute(t *testing.T) {
+	a := New(1)
+	if a.WasOn(1) != true {
+		t.Errorf("should have found it instead: %v", a.WasOn(1))
+	}
+}
+
+func TestShouldVerifyLastMinute(t *testing.T) {
+	a := New(0)
+	a.AddMinute(60)
+	if r := a.WasOn(60) != true; r {
+		t.Errorf("should have found it instead: %v", r)
+	}
+}
+
+func TestShouldVerifyMiddleMinute(t *testing.T) {
+	a := New(0)
+	a.AddMinute(30)
+	if r := a.WasOn(30) != true; r {
+		t.Errorf("should have found it instead: %v", r)
+	}
+}
+
+func TestShouldGetCorrectRanges(t *testing.T) {
+	//11100011100
+	a := New(1820)
+	ranges := a.GetRanges()
+	expected := []Tuple{NewTuple(3, 5), NewTuple(9, 11)}
+	if len(ranges) != len(expected) {
+		t.Errorf("length should have been %v, was %v", len(expected), len(ranges))
+	}
+	for i := range ranges {
+		if ranges[i] != expected[i] {
+			t.Errorf("element should have been %v, was %v", expected[i], ranges[i])
+		}
+	}
+}
+
+func TestShouldGetCorrectRangesFirstBit(t *testing.T) {
+	//111
+	a := New(7)
+	ranges := a.GetRanges()
+	expected := []Tuple{NewTuple(1, 3)}
+	if len(ranges) != len(expected) {
+		t.Errorf("length should have been %v, was %v", len(expected), len(ranges))
+	}
+	for i := range ranges {
+		if ranges[i] != expected[i] {
+			t.Errorf("element should have been %v, was %v", expected[i], ranges[i])
+		}
+	}
+}
+
+func TestShouldGetCorrectRangesJustFirstBit(t *testing.T) {
+	//1
+	a := New(1)
+	ranges := a.GetRanges()
+	expected := []Tuple{NewTuple(1, 1)}
+	if len(ranges) != len(expected) {
+		t.Errorf("length should have been %v, was %v", len(expected), len(ranges))
+	}
+	for i := range ranges {
+		if ranges[i] != expected[i] {
+			t.Errorf("element should have been %v, was %v", expected[i], ranges[i])
+		}
+	}
+}
+
+func TestShouldGetCorrectRangesLastBit(t *testing.T) {
+	a := New(0)
+	a.AddMinute(60)
+	a.AddMinute(59)
+	a.AddMinute(58)
+	ranges := a.GetRanges()
+	expected := []Tuple{NewTuple(58, 60)}
+	if len(ranges) != len(expected) {
+		t.Errorf("length should have been %v, was %v", len(expected), len(ranges))
+	}
+	for i := range ranges {
+		if ranges[i] != expected[i] {
+			t.Errorf("element should have been %v, was %v", expected[i], ranges[i])
+		}
+	}
+}
+
+func TestShouldGetCorrectRangesJustLastBit(t *testing.T) {
+	a := New(0)
+	a.AddMinute(60)
+	ranges := a.GetRanges()
+	expected := []Tuple{NewTuple(60, 60)}
+	if len(ranges) != len(expected) {
+		t.Errorf("length should have been %v, was %v", len(expected), len(ranges))
+	}
+	for i := range ranges {
+		if ranges[i] != expected[i] {
+			t.Errorf("element should have been %v, was %v", expected[i], ranges[i])
+		}
+	}
+}
